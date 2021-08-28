ecd5f50f378c96648a33e719cf7d1aa3197d5bcd
toruuetani <toruuetani@gmail.com>
2021-08-28 17:40:52 +0900

    Fix: cipher

--- .github/decrypt.sh
+++ .github/decrypt.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-openssl aes-256-cbc --pbkdf2 -d -in .github/.ssh/id_rsa-cipher -k ${KEY} >> .github/.ssh/id_rsa
+openssl aes-256-cbc --pbkdf2 -d -in .github/.ssh/id_rsa.cipher -k ${KEY} > .github/.ssh/id_rsa
--- .github/encrypt.sh
+++ .github/encrypt.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-openssl aes-256-cbc -pbkdf2 -e -in id_rsa -out ./id_rsa.cipher -k "${KEY}"
+openssl aes-256-cbc -pbkdf2 -e -in .github/.ssh/id_rsa -out .github/.ssh/id_rsa.cipher -k "${KEY}"
